59e23fdabb563629bd27ea113f0eb0f33b5f0382
YASH MAKWANA <72153542+Yashmakwanab@users.noreply.github.com>
2022-11-17 13:23:03 +0530

    contact v2

--- src/assets/pages/Contact/Contact.js
+++ src/assets/pages/Contact/Contact.js
@@ -1,6 +1,6 @@
-import './contact.scss';
-import React from 'react';
-import { contact } from '../../Data';
+import "./contact.scss";
+import React from "react";
+import { contact } from "../../Data";
 
 const Contact = () => {
   return (
@@ -9,14 +9,14 @@ const Contact = () => {
         <div className="line"></div>
         <a href={`mailto:${contact.email}`} target="blank">
           <div type="button" className="email">
-            Vaghelaabhishek2580@gmail.com
+            {contact.email}
           </div>
         </a>
         <div className="line"></div>
         <div className="call-section">
           <div className="call">call</div>
           <div className="call-number">
-            <a href="tel:8200-394-360">8200-394-360</a>
+            <a href="tel:8200-394-360">{contact.call}</a>
           </div>
         </div>
         <div className="line"></div>
