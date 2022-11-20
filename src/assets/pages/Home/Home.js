34e9c42620a92eaf986fa0a5f7cc7243765dad8f
Abhishek Vaghela <vaghelaabhshek2580@gmail.com>
2022-11-20 09:58:12 +0530

    about v3

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import './home.scss';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import Hero from '../../components/Hero';
 import Projects from '../Projects/Projects';
 import About from '../About/About';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+
 // import Feature from '../Feature/Feature';
 // import Contact from '../Contact/Contact';
 
 const Home = () => {
+  const containerRef = useRef(null);
+
   return (
     <div>
       <Hero />
