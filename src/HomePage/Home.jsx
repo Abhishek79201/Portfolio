a9d52ead378acda7d58af5e86f18cca0d08c75e3
YASH MAKWANA <72153542+Yashmakwanab@users.noreply.github.com>
2022-12-03 23:27:49 +0530

    navbar

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Navbar from '../assets/pages/Navbar/Navbar';
-import Hero from '../assets/pages/HeroSection/Hero';
-import About from '../assets/pages/About/About';
-import Projects from '../assets/pages/Projects/Projects';
+import React from "react";
+import Navbar from "../assets/pages/Navbar/Navbar";
+import Hero from "../assets/pages/HeroSection/Hero";
+import About from "../assets/pages/About/About";
+import Projects from "../assets/pages/Projects/Projects";
+import { Element } from "react-scroll";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
+      <React.Fragment>
+        <Element id="home" name="home">
+          <Navbar />
+          <Hero />
+          <About />
+          <Projects />
+        </Element>
+      </React.Fragment>
     </>
   );
 };
--- src/assets/pages/Projects/Projects.jsx
+++ src/assets/pages/Projects/Projects.jsx
@@ -1,11 +1,12 @@
-import './project.scss';
-import React from 'react';
-import { SectionHeader } from '../../components/SectionHeader/sectionHeader';
-import { Link } from 'react-router-dom';
-import { PrData } from './../../components/ProjectData/ProjectData';
-import { motion } from 'framer-motion';
-import { useLayoutEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import "./project.scss";
+import React from "react";
+import { Element } from "react-scroll";
+import { SectionHeader } from "../../components/SectionHeader/sectionHeader";
+import { Link } from "react-router-dom";
+import { PrData } from "./../../components/ProjectData/ProjectData";
+import { motion } from "framer-motion";
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
 const Wrapper = ({ children }) => {
   const location = useLocation();
   useLayoutEffect(() => {
@@ -18,59 +19,68 @@ const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 function Whereto(data) {
   if (data.id === 1) {
-    return '/project/pr1';
+    return "/project/pr1";
   }
   if (data.id === 2) {
-    return '/project/pr2';
+    return "/project/pr2";
   }
   if (data.id === 3) {
-    return '/project/pr3';
+    return "/project/pr3";
   }
   if (data.id === 4) {
-    return '/project/pr4';
+    return "/project/pr4";
   }
 }
 
 export default function Projects() {
   // console.log(images.length);
   return (
-    <Wrapper id="Project">
-      <section className="section-wrapper  gallery-wrap">
-        <div className="jsd">
-          <SectionHeader title="Projects" />
-        </div>
+    <React.Fragment>
+      <Element id="Project" name="Project">
+        <Wrapper id="Project">
+          <section className="section-wrapper  gallery-wrap">
+            <div className="jsd">
+              <SectionHeader title="Projects" />
+            </div>
 
-        <motion.div className="gallery">
-          {PrData.map((data) => (
-            <motion.div
-              className="pr"
-              exit={{ opacity: 0 }}
-              transition={transition}
-            >
-              <div className="titlediv">
-                <p className="title">{data.title}</p>
-              </div>
-              <div className="hj">
-                <Link to={Whereto(data)}>
-                  <p className="title-num">0{data.id}</p>
+            <motion.div className="gallery">
+              {PrData.map((data) => (
+                <motion.div
+                  className="pr"
+                  exit={{ opacity: 0 }}
+                  transition={transition}
+                >
+                  <div className="titlediv">
+                    <p className="title">{data.title}</p>
+                  </div>
+                  <div className="hj">
+                    <Link to={Whereto(data)}>
+                      <p className="title-num">0{data.id}</p>
 
-                  <img src={data.src} alt="img" width="100%" height="100%" />
+                      <img
+                        src={data.src}
+                        alt="img"
+                        width="100%"
+                        height="100%"
+                      />
 
-                  <motion.div
-                    className="dwmenu"
-                    exit={{ opacity: 0 }}
-                    transition={transition}
-                  >
-                    <p className="subtitle">{data.subtitle}</p>
+                      <motion.div
+                        className="dwmenu"
+                        exit={{ opacity: 0 }}
+                        transition={transition}
+                      >
+                        <p className="subtitle">{data.subtitle}</p>
 
-                    <p className="category">{data.category}</p>
-                  </motion.div>
-                </Link>
-              </div>
+                        <p className="category">{data.category}</p>
+                      </motion.div>
+                    </Link>
+                  </div>
+                </motion.div>
+              ))}
             </motion.div>
-          ))}
-        </motion.div>
-      </section>
-    </Wrapper>
+          </section>
+        </Wrapper>
+      </Element>
+    </React.Fragment>
   );
 }
