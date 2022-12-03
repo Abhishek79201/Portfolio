7430c779e18f98eaca8ac2ac0f98affce9219116
Abhishek Vaghela <vaghelaabhishek2580@gmail.com>
2022-12-04 00:22:13 +0530

    new file format

--- src/assets/hooks/useLocoScroll.js
+++ src/assets/hooks/useLocoScroll.js
@@ -1,6 +1,5 @@
 import LocomotiveScroll from 'locomotive-scroll';
 import { useEffect } from 'react';
-import 'C:/Users/vaghe/Documents/Portfolio/node_modules/locomotive-scroll/src/locomotive-scroll.scss';
 export default function useLocoScroll() {
   useEffect(() => {
     return () => {
