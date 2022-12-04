12196da631bb2f8df519a22b5cf5ff46a6141431
YASH MAKWANA <72153542+Yashmakwanab@users.noreply.github.com>
2022-12-04 11:34:50 +0530

    remove lazyload

--- src/AnimatedRoute.js
+++ src/AnimatedRoute.js
@@ -1,17 +1,17 @@
-import React, { lazy } from 'react';
-import { AnimatePresence } from 'framer-motion';
-import { Routes, Route } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import Pr1 from './assets/pages/MorePrs/Pr1';
-import Pr2 from './assets/pages/MorePrs/Pr2';
-import Pr3 from './assets/pages/MorePrs/Pr3';
-import Pr4 from './assets/pages/MorePrs/Pr4';
+import React from "react";
+import { AnimatePresence } from "framer-motion";
+import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import Pr1 from "./assets/pages/MorePrs/Pr1";
+import Pr2 from "./assets/pages/MorePrs/Pr2";
+import Pr3 from "./assets/pages/MorePrs/Pr3";
+import Pr4 from "./assets/pages/MorePrs/Pr4";
 // import Footer from "./assets/pages/Footer/Footer";
-const Home = lazy(() => import('./HomePage/Home'));
-// const Projects = lazy(() => import("./assets/pages/Projects/Projects"));
-const About = lazy(() => import('./assets/pages/About/About'));
-const Contact = lazy(() => import('./assets/pages/Contact/Contact'));
-const More = lazy(() => import('./assets/pages/About/About'));
+import Home from "./HomePage/Home";
+import Projects from "./assets/pages/Projects/Projects";
+import About from "./assets/pages/About/About";
+import Contact from "./assets/pages/Contact/Contact";
+import More from "./assets/pages/About/About";
 
 const AnimatedRoute = () => {
   const location = useLocation();
@@ -23,7 +23,7 @@ const AnimatedRoute = () => {
           <Route path="/about" element={<About />} />
           <Route path="/more" element={<More />} />
           <Route path="/contact" element={<Contact />} />
-          {/* <Route path="/project" element={<Projects />} /> */}
+          <Route path="/project" element={<Projects />} />
           <Route path="/project/pr1" element={<Pr1 />} />
           <Route path="/project/pr2" element={<Pr2 />} />
           <Route path="/project/pr3" element={<Pr3 />} />
